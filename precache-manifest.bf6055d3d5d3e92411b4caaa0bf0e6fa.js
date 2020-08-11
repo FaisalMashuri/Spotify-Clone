2fdf173420aa44df979d6ea369457c5a8a75fc9f
FaisalMashuri <faisal.mashuri18143@student.unsika.ac.id>
2020-08-11 21:02:48 +0700

    Updates

---
+++ precache-manifest.bf6055d3d5d3e92411b4caaa0bf0e6fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c15d52f78daebe1304ab8a73ef724bc",
+    "url": "/Spotify-Clone/index.html"
+  },
+  {
+    "revision": "58e05a7e17bcf2f84b91",
+    "url": "/Spotify-Clone/static/css/main.0a4ffb90.chunk.css"
+  },
+  {
+    "revision": "45804c2abbfbda610a9f",
+    "url": "/Spotify-Clone/static/js/2.70277753.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Spotify-Clone/static/js/2.70277753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58e05a7e17bcf2f84b91",
+    "url": "/Spotify-Clone/static/js/main.28a7319b.chunk.js"
+  },
+  {
+    "revision": "e2c55bdaf66c265cca03",
+    "url": "/Spotify-Clone/static/js/runtime-main.0cbc733f.js"
+  }
+]);
